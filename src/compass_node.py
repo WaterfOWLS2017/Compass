9008f2315263ec828482df68eab47656c64e9602
roboboat <boat@roboboat>
2017-02-08 21:45:41 -0500

    unknown changes to the compass ROS node

--- src/compass_node.py
+++ src/compass_node.py
@@ -1,18 +1,19 @@
 import rospy
-from compass.msg import compass_heading
+from compass.msg import heading
 from os import environ
-from HMC25583L import HMC5883L
+from HMC5883L import HMC5883L
+from math import atan2, degrees
 
 # attempt to get publishing rate from environment
-rate = os.environ.get('COMPASS_PUBRATE')
+rate = environ.get('COMPASS_PUBRATE')
 if rate == None:
-    rate = os.environ.get('ROS_PUBRATE')
+    rate = environ.get('ROS_PUBRATE')
 if rate == None:
     rate = 10
 
 # Initialize and configure ROS node and publisher
 rospy.init_node('compass_node')
-compass_ch = rospy.Publisher('/compass', compass_heading, queue_size=2)
+compass_ch = rospy.Publisher('/compass', heading, queue_size=2)
 rate = rospy.Rate(rate)
 
 # Initialize and configure compass
@@ -21,8 +22,10 @@ compass = HMC5883L(1)
 while not rospy.is_shutdown():
     (X, Y, Z) = compass.read()
     # perform calculation to obtain heading in degrees off north
-    d = math.degrees(math.atan2(Y, X))    
+    d = degrees(atan2(Y, X))
+    if d < 0:
+        d = 360 + d
     # publish degrees
-    compass_ch.publish(degrees=d)
+    compass_ch.publish(heading=d)
     # sleep and repeat
     rate.sleep()
